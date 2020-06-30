30629cf3cb787a853361c88ac53fa015661bf5d5
saucecodee <ashawinta@gmail.com>
2020-06-30 10:50:42 +0100

    fix: add .env to gitignore

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require('express-async-errors')
-const http = require('http');
 const express = require('express');
 const app = express();
-const server = http.createServer(app);
 
 const preMiddlewares = require('./src/middlewares/preMiddlewares');
 const errorMiddlewares = require('./src/middlewares/errorMiddlewares');
-const routes = require('./src/routes');
+const apiRoutes = require('./src/routes');
 const databaseConfig = require('./src/config/db');
 const port = process.env.PORT;
 
 preMiddlewares(app);
 
-app.use('/api', routes())
+app.use('/api', apiRoutes())
 
 app.use('/', (req, res) => {
   res.status(200).sendFile(express.static("public/index.html"));
@@ -20,11 +18,11 @@ app.use('/', (req, res) => {
 
 errorMiddlewares(app)
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`::: server listening on port ${port}. Open via http://localhost:${port}/`);
   databaseConfig();
 });
 
-server.on('error', (error) => {
+app.on('error', (error) => {
   console.log(`::> an error occiurred in our server: \n ${error}`);
 });
